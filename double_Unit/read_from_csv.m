31329461f985d104814de301ec618209cb81a97a
Clark Xu <yilinxu@brandeis.edu>
2023-07-18 21:59:15 -0400

    Update read_from_csv.m

--- double_Unit/read_from_csv.m
+++ double_Unit/read_from_csv.m
@@ -1,20 +1,23 @@
-N = 2;
-[output, Nvec1, Nvec2] = read_bistable("stablestates(6,-5).csv", N);
+N = 5;
+[output, Nvec1, Nvec2] = read_bistable("5ss(6,-5).csv", N);
 stable_states = zeros(Nvec1, Nvec2);
 
-for M = 0:2
-    Nstate = factorial(N)/(factorial(N-M)*factorial(M))*bistable;
+for M = 0:N
+    Nstate = factorial(N)/(factorial(N-M)*factorial(M));
     disp(Nstate)
     stable_states = stable_states + Nstate*reshape(output(M+1,:,:), [Nvec1, Nvec2]);
 end
 
-figure(99), imagesc(stable_states);
+x = [0, -5];
+y = [0, 6];
+figure(99), imagesc(x,y,stable_states);
 set(gca,'YDir','normal'), xlabel("Wie0"), ylabel("Wee0");
-title("Num of stabel states, Wie0-Wee0 sweep, Wei0 = " + num2str(Wei0))
+title("Num of stabel states, Wie0-Wee0 sweep, Wei0 = " + num2str(Wei0)+"; N = "+num2str(N))
 c = colorbar;
 c.Label.String = "stable state number";
 
-
+% Function read the csv from given file name. The csv file contains the
+% bistable information for a system of N unit.
 function [output, Nvec1, Nvec2] = read_bistable(filename, N)
     
     A = readmatrix(filename);
